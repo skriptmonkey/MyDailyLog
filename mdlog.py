6702115d9489eb7e19260aceb725561e4e576bd9
Alvin Morris <alvin.morris@gmail.com>
2012-05-06 16:29:55 -0800

    Add mdlog.py

---
+++ mdlog.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#################################################################
+# MyDailyLog
+# Author: Alvin D. Morris
+# email: alvin.morris@gmail.com
+#
+# Command line utility for keeping track of daily productivity.
+#################################################################
+
+def main():
+    pass

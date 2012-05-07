5f927ce7f432e257bca2d00b3f2b208a5c58e186
Alvin Morris <alvin.morris@gmail.com>
2012-05-06 19:55:52 -0800

    Added methods for getting the date and folders.

--- mdlog.py
+++ mdlog.py
@@ -9,10 +9,35 @@
 #################################################################
 
 import os
+import datetime
 
 class MyDailyLog:
     def __init__(self):
-        self.homeFolder = os.getenv("HOME")
+        self.MDLFolder = os.getenv("HOME") + "/Dropbox/MyDailyLog"
+        self.rightNow = datetime.datetime.now()
+
+    def getTimeString(self):
+        timeString = self.rightNow.hour + ":" + self.rightNow.minute + ":"\
+               + self.rightNow.second
+        return timeString
+
+    def getDateString(self):
+        mYear = self.rightNow.year
+        
+        theMonths = ["January", "February", "March", "April", "May", "June",\
+                "July", "August", "September", "October", "November",\
+                "December"]
+        mMonth = self.rightNow.month + " - " + theMonths[self.rightNow.month]
+
+        mDay = self.rightNow.day
+
+        return mYear + "/" + mMonth + "/" + mDay
+
+
+
+    def checkDir(self, dirPath):
+        if not os.path.isdir(dirPath):
+            os.makedirs(dirPath)
 
 def main():
     pass

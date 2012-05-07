9632b12b8cb6e6464ad99858c416f25cbfd721ef
Alvin Morris <alvin.morris@gmail.com>
2012-05-06 23:36:32 -0800

    Added methods for creating folders and files.

--- mdlog.py
+++ mdlog.py
@@ -14,30 +14,54 @@ import datetime
 class MyDailyLog:
     def __init__(self):
         self.MDLFolder = os.getenv("HOME") + "/Dropbox/MyDailyLog"
-        self.rightNow = datetime.datetime.now()
+        self.rightNow = 0
+        self.MDLDir = ""
+        self.MDLFile = ""
 
     def getTimeString(self):
-        timeString = self.rightNow.hour + ":" + self.rightNow.minute + ":"\
-               + self.rightNow.second
-        return timeString
+        AmPm = "AM"
+        self.rightNow = datetime.datetime.now()
+        if self.rightNow.hour > 12:
+            AmPm = "PM"
+            mHour = str(self.rightNow.hour - 12)
+        if mHour < 10:
+            mhour = "0" + str(mHour)
+        if self.rightNow.minute < 10:
+            mMinute = "0" + str(self.rightNow.minute)
+        else:
+            mMinute = str(self.rightNow.minute)
+        if self.rightNow.second < 10:
+            mSecond = "0" + str(self.rightNow.second)
+        else:
+            mSecond = str(self.rightNow.second)
+
+        return mHour + ":" + mMinute + ":" + mSecond + " " + AmPm
 
     def getDateString(self):
-        mYear = self.rightNow.year
-        
+        self.rightNow = datetime.datetime.now()
+        mYear = str(self.rightNow.year)
         theMonths = ["January", "February", "March", "April", "May", "June",\
                 "July", "August", "September", "October", "November",\
                 "December"]
-        mMonth = self.rightNow.month + " - " + theMonths[self.rightNow.month]
+        mMonth = str(self.rightNow.month) + "." +\
+                theMonths[self.rightNow.month - 1]
 
-        mDay = self.rightNow.day
+        return mYear + "/" + mMonth
 
-        return mYear + "/" + mMonth + "/" + mDay
+    def getLogDir(self):
+        return self.MDLFolder + "/" + self.getDateString()
 
-
+    def getLogFile(self):
+        self.rightNow = datetime.datetime.now()
+        return self.getLogDir() + "/" + str(self.rightNow.day) + ".txt"
 
     def checkDir(self, dirPath):
         if not os.path.isdir(dirPath):
             os.makedirs(dirPath)
+
+    def newEntry(self, mEntry):
+        pass
+
 
 def main():
     pass
